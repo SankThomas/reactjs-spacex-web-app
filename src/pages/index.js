5225faf9f980a3ff7abbd275c56df29bbfbfb6c9
Thomas Sankara <thomassankara74@gmail.com>
2022-05-14 11:06:12 +0300

    entry point for pages folder

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Homepage from "./Homepage"
+import Capsules from "./Capsules"
+import Cores from "./Cores"
+import Crew from "./Crew"
+import SingleCrew from "./SingleCrew"
+import Dragons from "./Dragons"
+import SingleDragon from "./SingleDragon"
+import Landpads from "./Landpads"
+import SingleLandpad from "./SingleLandpad"
+
+export {
+  Homepage,
+  Capsules,
+  Cores,
+  Crew,
+  SingleCrew,
+  Dragons,
+  SingleDragon,
+  Landpads,
+  SingleLandpad,
+}
