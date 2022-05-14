753e1b016257bec2615d87a04a6a75e437e57b42
Thomas Sankara <thomassankara74@gmail.com>
2022-05-14 11:06:30 +0300

    set up react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Header from "./components/Header"
+import {
+  Homepage,
+  Capsules,
+  Cores,
+  Crew,
+  SingleCrew,
+  Dragons,
+  SingleDragon,
+  Landpads,
+  SingleLandpad,
+} from "./pages"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+          <Route path="/capsules" element={<Capsules />}></Route>
+          <Route path="/cores" element={<Cores />}></Route>
+          <Route path="/crew" element={<Crew />}></Route>
+          <Route path="/crew/:id" element={<SingleCrew />}></Route>
+          <Route path="/dragons" element={<Dragons />}></Route>
+          <Route path="/dragons/:id" element={<SingleDragon />}></Route>
+          <Route path="/landpads" element={<Landpads />}></Route>
+          <Route path="/landpads/:id" element={<SingleLandpad />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
-
-export default App;
