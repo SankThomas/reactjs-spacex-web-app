b692f7352bbf2093e77c2edfe8ca7928ef78fb6c
Thomas Sankara <thomassankara74@gmail.com>
2022-06-01 21:22:08 +0300

    get request for launchpads page

--- src/pages/Launchpads.js
+++ src/pages/Launchpads.js
@@ -1,5 +1,19 @@
-import React from "react"
+import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
+import LoadingState from "../components/LoadingState"
 
 export default function Launchpads() {
-  return <div>Launchpads</div>
+  const [launchPads, setLaunchPads] = useState(null)
+
+  useEffect(() => {
+    const fetchLaunchPads = async () => {
+      const res = await fetch(`https://api.spacexdata.com/v4/launchpads`)
+      const data = await res.json()
+      setLaunchPads(data)
+    }
+
+    fetchLaunchPads()
+  }, [])
+
+  return <>Launchpads</>
 }
